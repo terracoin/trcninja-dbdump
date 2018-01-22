2fbbda0473cd3dba03f5e36b8cedf8f03a5bc1fd
Alexandre Devilliers <elbereth@dashninja.pl>
2018-01-22 18:08:49 +0100

    Mon Jan 22 17:07:11 UTC 2018 - autoupdate

---
+++ dashninja-dbdump.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+DB_user=dmndbdump
+DB=dash_mnninja
+MAINDIR=./dump
+DIR=dashninja-dbdump-$(date +%Y%m%d%H%M%S)
+DUMPDIR=$MAINDIR/$DIR
+DUMPNAM=$DIR.tar.bz2
+FULLDUMPNAM=$MAINDIR/$DUMPNAM
+TIMESTAMP=$(date +%s)
+DATE=$(date -u)
+LINKSFILE=$(head links.md)
+HEADER=$(cat header.md)
+
+[ -n "$DUMPDIR" ] || DUMPDIR=.
+test -d $DUMPDIR || mkdir -p $DUMPDIR
+
+echo "Dumping tables into separate SQL command files for database '$DB' into dir=$DUMPDIR"
+
+tbl_count=0
+
+for t in $(mysql -NBA -u $DB_user -D $DB -e 'show tables') 
+do 
+    echo "DUMPING TABLE: $DB.$t"
+    mysqldump -u $DB_user --lock-tables=false $DB $t > $DUMPDIR/$t.sql
+    tbl_count=$(( tbl_count + 1 ))
+done
+
+echo "$tbl_count tables dumped from database '$DB' into dir=$DUMPDIR"
+
+echo "Compressing archive $DUMPNAM into $DUMPDIR"
+
+cd $MAINDIR
+tar cvjf $DUMPNAM $DIR
+cd ..
+
+echo "Calculating checksum of $DIRFIN/$DUMPNAM"
+
+SIZE=$(stat -c%s $FULLDUMPNAM)
+SHA1=$(sha1sum $FULLDUMPNAM | cut -f1 -d' ')
+SHA256=$(sha256sum $FULLDUMPNAM | cut -f1 -d' ')
+
+echo "Sending $DIRFIN/$DUMPNAM to transfer.sh"
+
+URL=$(curl --progress-bar --upload-file $FULLDUMPNAM https://transfer.sh/$DUMPNAM)
+
+echo "Cleaning up"
+rm $FULLDUMPNAM
+cd $MAINDIR
+rm -rf $DIR
+cd ..
+
+echo -e "$LINKSFILE\n| [$DATE]($URL) | $SIZE | $SHA256 | $SHA1 |" > links.md
+
+cat header.md links.md > README.md
--- header.md
+++ header.md
@@ -12,4 +12,3 @@ You can use this for DASH crypto-currency analysis or to kick-start a Dash Ninja
 ## Automatic Dump Files
 | Date | Size | SHA256 | SHA1 |
 |--|--|--|--|
-| [Mon Jan 22 17:07:11 UTC 2018](https://transfer.sh/mcLxJ/dashninja-dbdump-20180122180711.tar.bz2) | 197480163 | 2183b2cbf170d92979758e4b6378543fb9ee21a27ae3bccb34b601acaf8ae3be | 3098a8ea78af9af13adb331faf809bc0a76b0d37 |
